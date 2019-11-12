0663f38491db2eafc038e1a3888ffc36dc6acf4d
nstd <nstd.home@gmail.com>
2019-11-12 14:42:20 +0300

    fix

--- aws-cpp-sdk-ec2/include/aws/ec2/EC2_UnrealInterface.h
+++ aws-cpp-sdk-ec2/include/aws/ec2/EC2_UnrealInterface.h
@@ -1,18 +1,18 @@
 #pragma once
 
 #include <aws/ec2/EC2_EXPORTS.h>
-#include <string>
+#include "aws/ec2/model/InstanceType.h"
 
 namespace UE
 {
 	AWS_EC2_API void InitAPI    ();
 	AWS_EC2_API void ShutdownAPI();
 
-	AWS_EC2_API bool CreateInstance  (const std::string& InstanceName, const std::string& AmiID, const std::string& Region, std::string& OutputMsg);
+	AWS_EC2_API bool CreateInstance  (const char* InstanceName, const char* AmiID, Aws::EC2::Model::InstanceType InstanceType, const char* Region, char* OutputMsg, size_t OutputMsgSize);
 
-	AWS_EC2_API bool StartInstance   (const std::string& InstanceID, const std::string& Region, std::string& OutputMsg);
-	AWS_EC2_API bool StopInstance    (const std::string& InstanceID, const std::string& Region, std::string& OutputMsg);
+	AWS_EC2_API bool StartInstance   (const char* InstanceID, const char* Region, char* OutputMsg, size_t OutputMsgSize);
+	AWS_EC2_API bool StopInstance    (const char* InstanceID, const char* Region, char* OutputMsg, size_t OutputMsgSize);
 
-	AWS_EC2_API bool GetInstanceState(const std::string& InstanceID, const std::string& Region, std::string& OutputMsg, int& OutInstanceState);
-	AWS_EC2_API bool GetInstanceIP   (const std::string& InstanceID, const std::string& Region, std::string& OutputMsg, std::string& OutIp);
+	AWS_EC2_API bool GetInstanceState(const char* InstanceID, const char* Region, char* OutputMsg, size_t OutputMsgSize, int& OutInstanceState);
+	AWS_EC2_API bool GetInstanceIP   (const char* InstanceID, const char* Region, char* OutputMsg, size_t OutputMsgSize, char* OutIp, size_t OutIpSize);
 }
--- aws-cpp-sdk-ec2/source/EC2_UnrealInterface.cpp
+++ aws-cpp-sdk-ec2/source/EC2_UnrealInterface.cpp
@@ -11,6 +11,7 @@
 #include "aws/ec2/model/StartInstancesRequest.h"
 #include "aws/ec2/model/StopInstancesRequest.h"
 #include "aws/ec2/model/DescribeAddressesRequest.h"
+#include <iomanip>
 
 Aws::SDKOptions options;
 
@@ -28,21 +29,22 @@ namespace UE
 		Aws::ShutdownAPI(options);
 	}
 
-	bool CreateInstance(const std::string& InstanceName, const std::string& AmiID, const std::string& Region, std::string& OutputMsg)
+	bool CreateInstance(const char* InstanceName, const char* AmiID, Aws::EC2::Model::InstanceType InstanceType, const char* Region, char* OutputMsg, size_t OutputMsgSize)
 	{
-		char Outputbuffer[1024];
-		Aws::String instanceName = InstanceName.c_str();
-		Aws::String ami_id = AmiID.c_str();
+		//char Outputbuffer[1024];
+		Aws::String instanceName = InstanceName;
+		Aws::String ami_id = AmiID;
 
 		// snippet-start:[ec2.cpp.create_instance.code]
 		Aws::Client::ClientConfiguration clientConfiguration;
-		clientConfiguration.region = Region.c_str();
+		clientConfiguration.region = Region;
 
 		Aws::EC2::EC2Client ec2(clientConfiguration);
 
 		Aws::EC2::Model::RunInstancesRequest run_request;
 		run_request.SetImageId(ami_id.c_str());
-		run_request.SetInstanceType(Aws::EC2::Model::InstanceType::t2_micro);
+		//run_request.SetInstanceType(Aws::EC2::Model::InstanceType::t2_micro);
+		run_request.SetInstanceType(InstanceType);
 		run_request.SetMinCount(1);
 		run_request.SetMaxCount(1);
 
@@ -50,22 +52,20 @@ namespace UE
 
 		if (!run_outcome.IsSuccess())
 		{
-			snprintf(Outputbuffer, sizeof(Outputbuffer), "Failed to start ec2 instance %s based on ami_id %s : '%s'"
-				, InstanceName.c_str()
-				, AmiID.c_str()
+			snprintf(OutputMsg, OutputMsgSize, "Failed to start ec2 instance %s based on ami_id %s : '%s'"
+				, InstanceName
+				, AmiID
 				, run_outcome.GetError().GetMessage().c_str());
-			OutputMsg = Outputbuffer;
 			return false;
 		}
 
 		const auto& instances = run_outcome.GetResult().GetInstances();
 		if (instances.size() == 0)
 		{
-			snprintf(Outputbuffer, sizeof(Outputbuffer), "Failed to start ec2 instance %s based on ami_id %s : '%s'"
-				, InstanceName.c_str()
-				, AmiID.c_str()
+			snprintf(OutputMsg, OutputMsgSize, "Failed to start ec2 instance %s based on ami_id %s : '%s'"
+				, InstanceName
+				, AmiID
 				, run_outcome.GetError().GetMessage().c_str());
-			OutputMsg = Outputbuffer;
 			return false;
 		}
 		// snippet-end:[ec2.cpp.create_instance.code]
@@ -83,127 +83,93 @@ namespace UE
 		auto create_outcome = ec2.CreateTags(create_request);
 		if (!create_outcome.IsSuccess())
 		{
-			snprintf(Outputbuffer, sizeof(Outputbuffer), "Failed to tag ec2 instance %s based on ami_id %s : '%s'"
+			snprintf(OutputMsg, OutputMsgSize, "Failed to tag ec2 instance %s based on ami_id %s : '%s'"
 				, instance_id.c_str()
-				, AmiID.c_str()
+				, AmiID
 				, create_outcome.GetError().GetMessage().c_str());
-			OutputMsg = Outputbuffer;
 			return false;
 		}
 
-		snprintf(Outputbuffer, sizeof(Outputbuffer), "Successfully started ec2 instance %s(%s) based on ami_id '%s'"
-			, InstanceName.c_str()
+		snprintf(OutputMsg, OutputMsgSize, "Successfully started ec2 instance %s(%s) based on ami_id '%s'"
+			, InstanceName
 			, instance_id.c_str()
-			, AmiID.c_str());
-		OutputMsg = Outputbuffer;
+			, AmiID);
 
 		return true;
 	}
 
-	bool StartInstance(const std::string& InstanceID, const std::string& Region, std::string& OutputMsg)
+	bool StartInstance(const char* InstanceID, const char* Region, char* OutputMsg, size_t OutputMsgSize)
 	{
-		char Outputbuffer[1024];
-		Aws::String instance_id = InstanceID.c_str();
+		Aws::String instance_id = InstanceID;
 		// snippet-start:[ec2.cpp.start_instance.code]
 		Aws::Client::ClientConfiguration clientConfiguration;
-		clientConfiguration.region = Region.c_str();
+		clientConfiguration.region = Region;
 
 		Aws::EC2::EC2Client ec2(clientConfiguration);
 
 		Aws::EC2::Model::StartInstancesRequest start_request;
-		start_request.AddInstanceIds(instance_id.c_str());
-		start_request.SetDryRun(true);
+		start_request.AddInstanceIds(instance_id);
 
-		auto dry_run_outcome = ec2.StartInstances(start_request);
-		if (!dry_run_outcome.IsSuccess() || dry_run_outcome.GetError().GetErrorType() != Aws::EC2::EC2Errors::DRY_RUN_OPERATION)
-		{
-			snprintf(Outputbuffer, sizeof(Outputbuffer), "Failed dry run to start instance %s : '%s'"
-				, instance_id.c_str()
-				, dry_run_outcome.GetError().GetMessage().c_str());
-			OutputMsg = Outputbuffer;
-			return false;
-		}
-
-		start_request.SetDryRun(false);
 		auto start_instancesOutcome = ec2.StartInstances(start_request);
 
 		if (!start_instancesOutcome.IsSuccess())
 		{
-			snprintf(Outputbuffer, sizeof(Outputbuffer), "Failed to start instance %s : '%s'"
+			snprintf(OutputMsg, OutputMsgSize, "Failed to start instance %s : '%s'"
 				, instance_id.c_str()
 				, start_instancesOutcome.GetError().GetMessage().c_str());
-			OutputMsg = Outputbuffer;
 			return false;
 		}
 
-		snprintf(Outputbuffer, sizeof(Outputbuffer), "Successfully started instance '%s'"
+		snprintf(OutputMsg, OutputMsgSize, "Successfully started instance '%s'"
 			, instance_id.c_str());
-		OutputMsg = Outputbuffer;
 		return true;
 	}
 
-	bool StopInstance(const std::string& InstanceID, const std::string& Region, std::string& OutputMsg)
+	bool StopInstance(const char* InstanceID, const char* Region, char* OutputMsg, size_t OutputMsgSize)
 	{
-		char Outputbuffer[1024];
-		Aws::String instance_id = InstanceID.c_str();
+		Aws::String instance_id = InstanceID;
 
 		// snippet-start:[ec2.cpp.stop_instance.code]
 		Aws::Client::ClientConfiguration clientConfiguration;
-		clientConfiguration.region = Region.c_str();
+		clientConfiguration.region = Region;
 
 		Aws::EC2::EC2Client ec2(clientConfiguration);
 
 		Aws::EC2::Model::StopInstancesRequest request;
-		request.AddInstanceIds(instance_id.c_str());
-		request.SetDryRun(true);
+		request.AddInstanceIds(instance_id);
 
-		auto dry_run_outcome = ec2.StopInstances(request);
-		if (!dry_run_outcome.IsSuccess() || dry_run_outcome.GetError().GetErrorType() != Aws::EC2::EC2Errors::DRY_RUN_OPERATION)
-		{
-			snprintf(Outputbuffer, sizeof(Outputbuffer), "Failed dry run to stop instance %s : %s"
-				, instance_id.c_str()
-				, dry_run_outcome.GetError().GetMessage().c_str());
-			OutputMsg = Outputbuffer;
-			return false;
-		}
-
-		request.SetDryRun(false);
 		auto outcome = ec2.StopInstances(request);
 		if (!outcome.IsSuccess())
 		{
-			snprintf(Outputbuffer, sizeof(Outputbuffer), "Failed to stop instance %s : %s"
+			snprintf(OutputMsg, OutputMsgSize, "Failed to stop instance %s : %s"
 				, instance_id.c_str()
 				, outcome.GetError().GetMessage().c_str());
-			OutputMsg = Outputbuffer;
 			return false;
 		}
 
-		snprintf(Outputbuffer, sizeof(Outputbuffer), "Successfully stopped instance %s"
+		snprintf(OutputMsg, OutputMsgSize, "Successfully stopped instance %s"
 			, instance_id.c_str());
-		OutputMsg = Outputbuffer;
 		return true;
 
 		// snippet-end:[ec2.cpp.stop_instance.code]
 	}
 
-	bool GetInstanceState(const std::string& InstanceID, const std::string& Region, std::string& OutputMsg, int& OutInstanceState)
+	bool GetInstanceState(const char* InstanceID, const char* Region, char* OutputMsg, size_t OutputMsgSize, int& OutInstanceState)
 	{
-		char Outputbuffer[1024];
-		Aws::String instance_id = InstanceID.c_str();
+		Aws::String instance_id = InstanceID;
 
 		Aws::Client::ClientConfiguration clientConfiguration;
-		clientConfiguration.region = Region.c_str();
+		clientConfiguration.region = Region;
 
 		Aws::EC2::EC2Client ec2(clientConfiguration);
 
 		Aws::EC2::Model::DescribeInstancesRequest request;
-		request.AddInstanceIds(instance_id.c_str());
+		request.AddInstanceIds(instance_id);
 
 		auto outcome = ec2.DescribeInstances(request);
 		if (!outcome.IsSuccess())
 		{
-			snprintf(Outputbuffer, sizeof(Outputbuffer), "Failed to describe ec2 instances: '%s'", outcome.GetError().GetMessage().c_str());
-			OutputMsg = Outputbuffer;
+			snprintf(OutputMsg, OutputMsgSize, "Failed to describe ec2 instances: '%s'", outcome.GetError().GetMessage().c_str());
 			return false;
 		}
 
@@ -217,38 +183,33 @@ namespace UE
 		return true;
 	}
 
-	bool GetInstanceIP(const std::string& InstanceID, const std::string& Region, std::string& OutputMsg, std::string& OutIp)
+	bool GetInstanceIP(const char* InstanceID, const char* Region, char* OutputMsg, size_t OutputMsgSize, char* OutIp, size_t OutIpSize)
 	{
-		char Outputbuffer[1024];
-		Aws::String instance_id = InstanceID.c_str();
+		Aws::String instance_id = InstanceID;
 
 		Aws::Client::ClientConfiguration clientConfiguration;
-		clientConfiguration.region = Region.c_str();
+		clientConfiguration.region = Region;
 
 		Aws::EC2::EC2Client ec2(clientConfiguration);
-		Aws::EC2::Model::DescribeAddressesRequest request;
 
-		auto outcome = ec2.DescribeAddresses(request);
+		Aws::EC2::Model::DescribeInstancesRequest request;
+		request.AddInstanceIds(instance_id);
+
+		auto outcome = ec2.DescribeInstances(request);
 		if (!outcome.IsSuccess())
 		{
-			snprintf(Outputbuffer, sizeof(Outputbuffer), "Failed to describe elastic ip addresses: '%s'"
-				, outcome.GetError().GetMessage().c_str());
-			OutputMsg = Outputbuffer;
+			snprintf(OutputMsg, OutputMsgSize, "Failed to describe ec2 instances: '%s'", outcome.GetError().GetMessage().c_str());
 			return false;
 		}
 
-		const auto &addresses = outcome.GetResult().GetAddresses();
-		auto address = std::find_if(addresses.begin(), addresses.end(), [instance_id](const Aws::EC2::Model::Address& Item) { return Item.GetInstanceId() == instance_id;  });
-		if (address == addresses.end())
-		{
-			snprintf(Outputbuffer, sizeof(Outputbuffer), "Ip not found - instance '%s'"
-				, instance_id.c_str());
-			OutputMsg = Outputbuffer;
-			return true;
-		}
+		const auto &reservations = outcome.GetResult().GetReservations();
+		JCHECK(reservations.size() == 1);
+		auto reservation = reservations[0];
+		auto instances = reservation.GetInstances();
+		JCHECK(instances.size() == 1);
 
-		OutIp = address->GetPublicIp().c_str();
+		const Aws::String& Ip = instances[0].GetPublicIpAddress();
+		strncpy_s(OutIp, OutIpSize, Ip.c_str(), Ip.size());
 		return true;
 	}
-
 }
